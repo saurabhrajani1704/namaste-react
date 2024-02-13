151e968becc074d1e7eca4c18ea630366a7253b2
Saurabh Rajani <saurabhrajani1704@gmail.com>
2024-02-13 13:07:14 +0530

    new file:   ASSIGNMENTS/ASSIGNMENT3.txt 	modified:   App.js 	modified:   package.json

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom" 
-const parent=React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement(
-        "div",
-        {id:"child"},
-        [React.createElement("h1",{},"I am h1 tag"),
-        React.createElement("h2",{},"I am h2 tag")  
-        ])
+import ReactDOM from "react-dom/client" ;
+
+
+
+//JSX (transpiled before it reaches the JS)- PARCEL - Babel
+
+//JSX => Babel Transpiles it to React.CreateElement => ReactElement =>JsObject => HtmlElement(render)
+const title= (
+    <h1 className="head">
+        Namaste React using JSX
+    </h1>
 );
-console.log(parent);
 
-const heading=React.createElement(
-    "h1",
-    {id:"heading"},
-    "Hello World from React!");
+//React Components
+//Component Composition
+  const HeadingComponent=() => (
+    <div id="container">
+        {title}
+        <h1>Namaste React from Functional Component </h1>
+    </div>
+  );
 
+  
 const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
  
-root.render(parent) ;
